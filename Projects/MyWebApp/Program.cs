1f474aa277bed90ff5b2edfa6942aafc140b7968
unknown <tpham20908@gmail.com>
2018-03-07 15:03:57 -0500

    MyWebApp - Add file Product.cs in Models folder

--- Projects/MyWebApp/Program.cs
+++ Projects/MyWebApp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace HelloMVC
+namespace MyWebApp
 {
     public class Program
     {
