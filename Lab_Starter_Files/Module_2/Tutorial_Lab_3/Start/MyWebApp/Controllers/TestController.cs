f563586763f09863cc166efa96912c82d9163f8b
unknown <tpham20908@gmail.com>
2018-03-14 21:54:03 -0400

    Tutorial Lab 2 - Loosely-Typed View Models

---
+++ Lab_Starter_Files/Module_2/Tutorial_Lab_3/Start/MyWebApp/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+namespace MyWebApp.Controllers 
+{
+    public class TestController : Controller 
+    {
+        public IActionResult Foo() 
+        {
+            ViewData["Title"] = "Foo";
+            ViewData["Header"] = "Header Foo";
+            ViewData["Items"] = "AAA BBB CCC".Split(' ');
+            return View();
+        }
+    }
+}
