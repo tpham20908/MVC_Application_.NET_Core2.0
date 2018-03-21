981bd6a496f5e5709fbb683041315a5e88b60b3b
unknown <tpham20908@gmail.com>
2018-03-20 20:57:42 -0400

    Get model's property name with generic classes and lambda expression

---
+++ FakeHtmlHelper/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeHtmlHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var view = new FakeViewPage<Person>();
+            var propertyName = view.HtmlHelper.GetName(p => p.ID);
+            Console.WriteLine(propertyName);
+            propertyName = view.HtmlHelper.GetName(p => p.Name);
+            Console.WriteLine(propertyName);
+            propertyName = view.HtmlHelper.GetName(p => p.Age);
+            Console.WriteLine(propertyName);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public  int Age { get; set; }
+    }
+
+    public class FakeHtmlHelper<TModel>
+    {
+
+    }
+
+    public class FakeViewPage<TModel>
+    {
+        public FakeHtmlHelper<TModel> HtmlHelper { get; set; }
+        public FakeViewPage()
+        {
+            HtmlHelper = new FakeHtmlHelper<TModel>();
+        }
+    }
+
+    public static class FakeHtmlHelperExtension
+    {
+        public static string GetName<TModel, TResult>(this FakeHtmlHelper<TModel> target,
+            Expression<Func<TModel, TResult>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+            return body.Member.Name;
+        }
+    }
+}
