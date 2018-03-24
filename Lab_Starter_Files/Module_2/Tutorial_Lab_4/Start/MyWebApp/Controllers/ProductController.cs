c6cc3a216af25b7cf85e076706f53b6bd429ac8d
unknown <tpham20908@gmail.com>
2018-03-24 09:12:25 -0400

    Html helper with product/edit sample

--- Lab_Starter_Files/Module_2/Tutorial_Lab_4/Start/MyWebApp/Controllers/ProductController.cs
+++ Lab_Starter_Files/Module_2/Tutorial_Lab_4/Start/MyWebApp/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using  System.Collections.Generic;
 
 namespace MyWebApp.Controllers {
     public class ProductController : Controller {
+        /*
         public IActionResult Index() {
             var vm = new ProductListVM();
             vm.Discount = new Discount { Start = DateTime.Today, End = DateTime.Today.AddDays(30), Rate = 0.75 };
@@ -13,6 +14,22 @@ namespace MyWebApp.Controllers {
             vm.Products.Add(new Product { ID = 102, Name = "Bike", Price = 30 });
             vm.Products.Add(new Product { ID = 103, Name = "Fireworks", Price = 40 });
             return View("ProductList", vm);
+        }
+        */
+
+        // Edit single product
+        public IActionResult Edit() {
+            var product = new Product(901, "Rocket", 99.99);
+            return View(product);
+        }
+
+        // Show a product list
+        public IActionResult Show() {
+            var products = new List<Product>();
+            products.Add(new Product(101, "Apple", 19.99));
+            products.Add(new Product(201, "Bike", 29.99));
+            products.Add(new Product(301, "Couch", 39.99));
+            return View(products);
         }
     }
 }
--- Lab_Starter_Files/Module_2/Tutorial_Lab_4/Start/MyWebApp/Models/Source.cs
+++ Lab_Starter_Files/Module_2/Tutorial_Lab_4/Start/MyWebApp/Models/Source.cs
@@ -43,6 +43,11 @@ namespace MyWebApp.Models {
         public int ID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public string Description { get; set; }
+
+        public Product(int id, string name, double price) {
+            this.ID = id; this.Name = name; this.Price = price;
+        }
     }
 
     public class Discount 
