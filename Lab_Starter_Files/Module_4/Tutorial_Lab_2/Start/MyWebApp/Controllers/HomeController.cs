8837bb0afb085cf5a23bfec9bf11498e899bcc0d
unknown <tpham20908@gmail.com>
2018-03-25 14:43:28 -0400

    Module 4: Assessment Lab 1 - Create HTML Helper for table

--- Lab_Starter_Files/Module_4/Tutorial_Lab_2/Start/MyWebApp/Controllers/HomeController.cs
+++ Lab_Starter_Files/Module_4/Tutorial_Lab_2/Start/MyWebApp/Controllers/HomeController.cs
@@ -3,10 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 namespace MyWebApp {
     public class HomeController : Controller {
         public IActionResult Index() {
+            
             ViewData["country"] = "United States";
             ViewData["state"] = "Washington";
             ViewData["county"] = "King";
             ViewData["city"] = "Redmond";
+            
+            ViewData["columnNames"] = new string[] { "ID", "Name", "Price" };
+            ViewData["content"] = new string[,]{
+                {"101", "Apple", "1.01"},
+                {"202", "Back", "2.02"},
+                {"303", "Cup", "3.03"},
+                {"404", "Donut", "3.03"}
+            };
             return View();
         }
     }
--- Lab_Starter_Files/Module_4/Tutorial_Lab_2/Start/MyWebApp/Extensions/CustomHtmlHelper.cs
+++ Lab_Starter_Files/Module_4/Tutorial_Lab_2/Start/MyWebApp/Extensions/CustomHtmlHelper.cs
@@ -11,5 +11,30 @@ namespace Microsoft.AspNetCore.Mvc.Rendering {
             tagBuilder.InnerHtml.Append(content ?? string.Empty);
             return tagBuilder;
         }
+
+        public static IHtmlContent Table(this IHtmlHelper htmlHelper, string[] columnNames, string[,] content) {
+            var tableBuilder = new TagBuilder("table");
+            tableBuilder.Attributes.Add("border", "1");
+            var headerBuilder = new TagBuilder("tr");
+            foreach (var cn in columnNames) {
+                var headerCellBuilder = new TagBuilder("th");
+                headerCellBuilder.InnerHtml.Append(cn);
+                headerBuilder.InnerHtml.AppendHtml(headerCellBuilder);
+            }
+
+            tableBuilder.InnerHtml.AppendHtml(headerBuilder);
+            int colCount = columnNames.Length;
+            int rowCount = content.Length / colCount;
+            for (int r = 0; r < rowCount; r++) {
+                var rowBuilder = new TagBuilder("tr");
+                for (int c = 0; c < colCount; c++) {
+                    var cellBuilder = new TagBuilder("td");
+                    cellBuilder.InnerHtml.Append(content[r, c]);
+                    rowBuilder.InnerHtml.AppendHtml(cellBuilder);
+                }
+                tableBuilder.InnerHtml.AppendHtml(rowBuilder);
+            }
+            return tableBuilder;
+        }
     }
 }
