93cd96791fa763577a08cc7881379a136c821733
unknown <tpham20908@gmail.com>
2018-04-29 17:23:03 -0400

    LibraryContext class

---
+++ LibraryManagementSystem/LibraryData/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions option) : base (option) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
