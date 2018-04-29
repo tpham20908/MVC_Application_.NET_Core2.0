e2b4fbdbdfde1ce1682bbdf6ed32fc4e32aaee3e
unknown <tpham20908@gmail.com>
2018-04-29 16:08:46 -0400

    Created ConnectionString for database

---
+++ LibraryManagementSystem/Library/LibraryData/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext (DbContextOptions option) : base (option) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
