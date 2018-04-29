088c4474e138956337d9c6653c3a2ff399b65ee4
unknown <tpham20908@gmail.com>
2018-04-29 15:35:38 -0400

    LibraryManagementSystem project

---
+++ LibraryManagementSystem/LibraryData/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions option) : base (option) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
