fa24e33b9903c208d2bafb84cf2d58f34f6d6a6a
unknown <tpham20908@gmail.com>
2018-05-01 14:53:07 -0400

    services.AddScoped<ICheckout, CheckoutService> in Startup.cs

--- LibraryManagementSystem/Library/Startup.cs
+++ LibraryManagementSystem/Library/Startup.cs
@@ -27,6 +27,8 @@ namespace Library
             services.AddMvc();
             services.AddSingleton(Configuration);
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
+            services.AddScoped<ICheckout, CheckoutService>();
+
             services.AddDbContext<LibraryContext>(options
                 => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
