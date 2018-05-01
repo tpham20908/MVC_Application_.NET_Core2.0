40a6d4f01b7149fe636c0e800af18424ce92126e
unknown <tpham20908@gmail.com>
2018-04-30 21:46:14 -0400

    creating LibraryServices/CheckoutService.cs

---
+++ LibraryManagementSystem/LibraryServices/CheckoutService.cs
@@ -0,0 +1,70 @@
+﻿using LibraryData;
+using System;
+using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryServices
+{
+    public class CheckoutService : ICheckout
+    {
+        public void Add(Checkout newCheckout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckInItem(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOutItem(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Checkout> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Checkout GetById(int checkoutId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentHoldPatronName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCurrentHoldPlaced(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hold> GetCurrentHolds(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkFound(int assetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkLost(int assetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceHold(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
