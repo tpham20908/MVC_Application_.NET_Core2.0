23d3874b410dc02f566654bdde757eaaad9a7026
unknown <tpham20908@gmail.com>
2018-05-02 18:06:23 -0400

    practices with mvc

--- AutomatedTellerMachine/AutomatedTellerMachine/App_Start/RouteConfig.cs
+++ AutomatedTellerMachine/AutomatedTellerMachine/App_Start/RouteConfig.cs
@@ -14,10 +14,19 @@ namespace AutomatedTellerMachine
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Serial number",
+                url: "serial/{letterCase}",
+                defaults: new { controller = "Home", action = "Serial",
+                    letterCase = "upper" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index",
+                    id = UrlParameter.Optional }
             );
+
         }
     }
 }
--- AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -22,9 +22,29 @@ namespace AutomatedTellerMachine.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.TheMessage = "Having trouble, send us a message.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            //Todo: send message to HQ
+            ViewBag.TheMessage = "Thanks, we got your message.";
+
+            return View();
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+            if (letterCase == "lower")
+                return Content(serial.ToLower());
+            // return Content(serial);
+            // return new HttpStatusCodeResult(403);
+            // return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+            return RedirectToAction("About");
         }
     }
 }
