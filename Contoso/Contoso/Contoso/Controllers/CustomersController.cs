37d62187598ebca60a26672d837410d60cb416bb
tpham20908 <tpham20908@gmail.com>
2018-05-14 22:07:10 -0400

    used linq with EF

--- Contoso/Contoso/Contoso/Controllers/CustomersController.cs
+++ Contoso/Contoso/Contoso/Controllers/CustomersController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contoso.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Contoso.Controllers
 {
     public class CustomersController : Controller
     {
-        public IActionResult Index()
+        private AdventureWorksContext db = new AdventureWorksContext();
+
+        public IActionResult Index(int num)
         {
-            return View();
+            var person = db.Person.FirstOrDefault(p => p.BusinessEntityId == num);
+
+            return View(person);
+        }
+
+        public IActionResult PeopleList()
+        {
+            //var people = db.Person.Where(p => p.BusinessEntityId < 3612 && c.BusinessEntityId > 3493);
+            ICollection<Person> people = db.Person.ToList();
+            return View(people);
         }
     }
 }
