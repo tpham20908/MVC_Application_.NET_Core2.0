59f205340a99642683d4bf641d57d7330cd17bb3
tpham20908 <tpham20908@gmail.com>
2018-05-16 10:50:08 -0400

    created ViewComponent

---
+++ Contoso/Contoso/Contoso/ViewComponents/CustomerInfoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Contoso.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contoso.ViewComponents
+{
+    public class CustomerInfoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            AdventureWorksContext context = new AdventureWorksContext();
+            int rand = new Random().Next(1, 20000);
+            var cust = await context.Person.FirstOrDefaultAsync(c => c.BusinessEntityId == rand);
+
+            return View(cust);
+        }
+    }
+}
