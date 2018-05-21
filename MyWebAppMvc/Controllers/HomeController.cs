2f93700a212f2f76b7034f0cdc1061de9fbfe836
tpham20908 <tpham20908@gmail.com>
2018-05-20 22:45:22 -0400

    messed around with middleware

--- MyWebAppMvc/Controllers/HomeController.cs
+++ MyWebAppMvc/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace MyWebAppMvc.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
         {
             return View();
         }
