b1aa1cd54f01efe9ad66b1d4c1e80aae15e0bddc
tpham20908 <tpham20908@gmail.com>
2018-05-23 15:05:27 -0400

    created CatalogController

--- LibraryManagementSystem/Library/Controllers/CatalogController.cs
+++ LibraryManagementSystem/Library/Controllers/CatalogController.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using Library.Models.Catalog;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.Controllers
 {
-    public class CatalogController
+    public class CatalogController : Controller
     {
+        private ILibraryAsset _assets;
+
+        public CatalogController(ILibraryAsset assets)
+        {
+            _assets = assets;
+        }
+
+        public IActionResult Index()
+        {
+            var assetModels = _assets.GetAll();
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl = result.ImageUrl,
+                    Title = result.Title,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _assets.GetDeweyIndex(result.Id),
+                    Type = _assets.GetType(result.Id)
+                });
+
+            return View(listingResult);
+        }
     }
 }
