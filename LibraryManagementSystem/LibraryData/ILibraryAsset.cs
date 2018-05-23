25b559fd1897b10d1f53a97f47ec84861692273c
tpham20908 <tpham20908@gmail.com>
2018-05-23 13:53:41 -0400

    created interface class ILibraryAsset

---
+++ LibraryManagementSystem/LibraryData/ILibraryAsset.cs
@@ -0,0 +1,20 @@
+﻿using LibraryData.Models;
+using System.Collections.Generic;
+
+namespace LibraryData
+{
+    public interface ILibraryAsset
+    {
+        IEnumerable<LibraryAsset> GetAll();
+        ILibraryAsset GetById(int id);
+
+        void Add(LibraryAsset newAsset);
+        string GetAuthorOrDirector(int id);
+        string GetDeweyIndex(int id);
+        string GetType(int id);
+        string GetTitle(int id);
+        string GetIsbn(int id);
+
+        LibraryBranch GetCurrentLocation(int id);
+    }
+}
