0ef22df07042305c0273f774f439240349e18ae6
tpham20908 <tpham20908@gmail.com>
2018-05-24 10:02:10 -0400

    created ICheckout.cs in LibraryData

---
+++ LibraryManagementSystem/LibraryData/ICheckout.cs
@@ -0,0 +1,23 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryData
+{
+    public interface ICheckout
+    {
+        IEnumerable<Checkout> GetAll();
+        Checkout GetById(int checkoutId);
+        void CheckOutItem(int assetId, int libraryCardId);
+        void CheckInItem(int assetId, int libraryCardId);
+        IEnumerable<CheckoutHistory> GetCheckoutHistories(int id);
+
+        void PlaceHold(int assetId, int libraryCardId);
+        string GetCurrentHoldPatronName(int id);
+        DateTime GetCurrentHoldPlaced(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+
+        void MarkLost(int assetId);
+        void MarkFound(int assetId);
+    }
+}
