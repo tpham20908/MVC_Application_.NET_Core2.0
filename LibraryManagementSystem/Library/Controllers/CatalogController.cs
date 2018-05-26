1644d67fcbc3f02adcec6f51bdf799b62c59432d
tpham20908 <tpham20908@gmail.com>
2018-05-25 22:26:34 -0400

    completed Detail method in CatalogController

--- LibraryManagementSystem/Library/Controllers/CatalogController.cs
+++ LibraryManagementSystem/Library/Controllers/CatalogController.cs
@@ -11,10 +11,12 @@ namespace Library.Controllers
     public class CatalogController : Controller
     {
         private ILibraryAsset _assets;
+        private ICheckout _checkouts;
 
-        public CatalogController(ILibraryAsset assets)
+        public CatalogController(ILibraryAsset assets, ICheckout checkouts)
         {
             _assets = assets;
+            _checkouts = checkouts;
         }
 
         public IActionResult Index()
@@ -42,10 +44,18 @@ namespace Library.Controllers
         {
             var asset = _assets.GetById(id);
 
+            var currentHolds = _checkouts.GetCurrentHolds(id)
+                .Select(a => new AssetHoldModel
+                {
+                    PatronName = _checkouts.GetCurrentHoldPatronName(a.Id),
+                    HoldPlaced = _checkouts.GetCurrentHoldPlaced(a.Id)
+                });
+
             var model = new AssetDetailModel
             {
                 AssetId = asset.Id,
                 Title = asset.Title,
+                Type = _assets.GetType(id),
                 Year = asset.Year,
                 Cost = asset.Cost,
                 Status = asset.Status.Name,
@@ -54,6 +64,10 @@ namespace Library.Controllers
                 CurrentLocation = _assets.GetCurrentLocation(id).Name,
                 DeweyCallNumber = _assets.GetDeweyIndex(id),
                 ISBN = _assets.GetIsbn(id),
+                CheckoutHistory = _checkouts.GetCheckoutHistories(id),
+                LatestCheckout = _checkouts.GetLatestCheckout(id),
+                PatronName = _checkouts.GetCurrentHoldPatronName(id),
+                CurrentHolds = currentHolds
             };
 
             return View(model);
