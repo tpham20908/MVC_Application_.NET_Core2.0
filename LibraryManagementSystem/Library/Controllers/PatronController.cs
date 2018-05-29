879a4cde65c714b01353f6d67ab40fb469b7e598
tpham20908 <tpham20908@gmail.com>
2018-05-28 21:36:31 -0400

    implemented Index method in PatronController

--- LibraryManagementSystem/Library/Controllers/PatronController.cs
+++ LibraryManagementSystem/Library/Controllers/PatronController.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Library.Models.PatronModels;
+using LibraryData;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Library.Controllers
 {
     public class PatronController : Controller
     {
+        private IPatron _patron;
+
+        public PatronController(IPatron patron)
+        {
+            _patron = patron;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var allPatrons = _patron.GetAll();
+
+            var patronModels = allPatrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                OverDueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.HomeLibraryBranch.Name
+            }).ToList();
+
+            var model = new PatronIndexModel
+            {
+                Patrons = patronModels
+            };
+
+            return View(model);
         }
     }
 }
---
+++ LibraryManagementSystem/Library/Models/PatronModels/PatronIndexModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Library.Models.PatronModels
+{
+    public class PatronIndexModel
+    {
+        public IEnumerable<PatronDetailModel> Patrons { get; set; }
+    }
+}
