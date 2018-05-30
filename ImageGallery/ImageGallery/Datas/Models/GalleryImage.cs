8e197e65441861160b63a737381b980d844c9788
tpham20908 <tpham20908@gmail.com>
2018-05-30 10:43:46 -0400

    created DbConnection obj in appsettings.json

--- ImageGallery/ImageGallery/Datas/Models/GalleryImage.cs
+++ ImageGallery/ImageGallery/Datas/Models/GalleryImage.cs
@@ -7,6 +7,7 @@ namespace ImageGallery.Datas.Models
 {
     public class GalleryImage
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Created { get; set; }
         public string Url { get; set; }
