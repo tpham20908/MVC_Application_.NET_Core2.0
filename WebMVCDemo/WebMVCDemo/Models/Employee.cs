84faf9138f6f87edf8899d9066b9b1cca0bdd15d
tpham20908 <tpham20908@gmail.com>
2018-06-10 21:41:07 -0400

    Added Data annotation

--- WebMVCDemo/WebMVCDemo/Models/Employee.cs
+++ WebMVCDemo/WebMVCDemo/Models/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebMVCDemo.Models
@@ -7,18 +8,24 @@ namespace WebMVCDemo.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Title of Courtesy")]
         public string TitleOfCourtesy { get; set; }
+        [Display(Name = "Birth day")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Hired date")]
         public DateTime HireDate { get; set; }
         public string Address { get; set; }
+        [Display(Name = "Home phone")]
         public string HomePhone { get; set; }
         public string City { get; set; }
         public string Region { get; set; }
         public string Country { get; set; }
+        [Display(Name = "Postal code")]
         public string PostalCode { get; set; }
-        public string Extension { get; set; }
     }
 }
