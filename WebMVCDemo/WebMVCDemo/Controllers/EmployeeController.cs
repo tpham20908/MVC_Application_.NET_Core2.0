882e046ba354e54e670a04b82459d72687f739a9
tpham20908 <tpham20908@gmail.com>
2018-06-10 20:57:14 -0400

    WebMVCDemo Data access EF

---
+++ WebMVCDemo/WebMVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCDemo.Models;
+
+namespace WebMVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Details(int id)
+        {
+            EmployeeContext _context = new EmployeeContext();
+            Employee employee = _context.Employees.Single(e => e.EmployeeId == id);
+
+            return View(employee);
+        }
+    }
+}
