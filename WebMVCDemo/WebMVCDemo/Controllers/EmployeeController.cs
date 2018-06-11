e3fa1f461ca379230ccbdbfeb2f7ae483a72c9cc
tpham20908 <tpham20908@gmail.com>
2018-06-10 21:27:53 -0400

    WebMVCDemo Details and Index

--- WebMVCDemo/WebMVCDemo/Controllers/EmployeeController.cs
+++ WebMVCDemo/WebMVCDemo/Controllers/EmployeeController.cs
@@ -9,12 +9,27 @@ namespace WebMVCDemo.Controllers
 {
     public class EmployeeController : Controller
     {
+        //private EmployeeContext _context;
+
+        //public EmployeeController(EmployeeContext context)
+        //{
+        //    _context = context;
+        //}
+
         public ActionResult Details(int id)
         {
             EmployeeContext _context = new EmployeeContext();
             Employee employee = _context.Employees.Single(e => e.EmployeeId == id);
 
             return View(employee);
+        }
+
+        public ActionResult Index()
+        {
+            EmployeeContext _context = new EmployeeContext();
+            List<Employee> employees = _context.Employees.ToList();
+
+            return View(employees);
         }
     }
 }
