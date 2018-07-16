10784812cd18e95793386d17de63b40bd2bf146c
tpham20908 <tpham20908@gmail.com>
2018-07-16 07:41:57 -0400

    removed unused projs

--- V2/Projects/MyWebApp/Models/Product.cs
+++
@@ -1,7 +0,0 @@
-namespace MyWebApp.Models {
-    public class Product {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-}
