62e0b9cceae3acb0a4a0798f2c49d73d25c79444
tpham20908 <tpham20908@gmail.com>
2018-07-19 11:28:05 -0400

    finished Task1

---
+++ WebService/Assessments/Lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[] {
+                new Product { ID = 2005, Name = "Tilapia", Price = 23.99 },
+                new Product { ID = 2006, Name = "Calmar", Price = 35.99 },
+                new Product { ID = 2007, Name = "Basa", Price = 18.99 },
+                new Product { ID = 2008, Name = "Salmon", Price = 32.99 },
+            };
+
+            string productsStr = DoSerialization(products);
+            System.Console.WriteLine(productsStr);
+            System.Console.WriteLine("==================");
+            DoDeserialization(productsStr);
+        }
+
+        static string DoSerialization(Product[] products) {
+            var productList = JsonConvert.SerializeObject(products);
+            return productList;
+        }
+
+        static void DoDeserialization(string productList) {
+            var products = JsonConvert.DeserializeObject<Product[]>(productList);
+
+            foreach (var product in  products) {
+                System.Console.WriteLine($"ID: {product.ID} Name: {product.Name} Price: {product.Price}");
+            }
+        }
+    }
+}
