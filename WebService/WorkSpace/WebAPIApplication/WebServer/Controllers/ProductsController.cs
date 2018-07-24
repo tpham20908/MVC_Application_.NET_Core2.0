e8a7cd46ed396ab82c0ac2d91de657a0620bf1c5
tpham20908 <tpham20908@gmail.com>
2018-07-23 21:45:59 -0400

    created action Get() in ProductsController

--- WebService/WorkSpace/WebAPIApplication/WebServer/Controllers/ProductsController.cs
+++ WebService/WorkSpace/WebAPIApplication/WebServer/Controllers/ProductsController.cs
@@ -1,5 +1,9 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class ProductsController : Controller {
-
+    [HttpGet]
+    public Product[] Get() {
+        return FakeData.Products.Values.ToArray();
+    }
 }
